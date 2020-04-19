f5a8d2382caa365ad9d673b0f6a5aca43a219905
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2020-04-18 22:34:11 -0700

    styling finalized

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import SingleProject from "../components/SingleProject";
 
-import data from "../utils/data";
+import Data from "../utils/data";
 
-const categories = [
+const Categories = [
   "All",
   "WebDev",
   "Frontend",
@@ -13,6 +13,22 @@ const categories = [
 ];
 
 const ProjectsPage = () => {
+  const [projectsShown, setProjectsShownAndCategory] = useState({
+    projectsArray: [...Data],
+    category: "All",
+  });
+
+  const changeCategory = (category) => {
+    const ProjectsShown = Data.filter(
+      (e) => e.category[1] === category || e.category[0] === category
+    );
+    setProjectsShownAndCategory({
+      ...ProjectsShown,
+      category: category,
+      projectsArray: ProjectsShown,
+    });
+  };
+
   return (
     <div className="w-full p-6 bg-blue-200">
       <div className="w-48 mx-auto pt-6 border-b-2 border-orange-500 text-center text-2xl text-blue-700">
@@ -22,17 +38,22 @@ const ProjectsPage = () => {
         I work on various side-projects.
       </div>
       <nav className="flex justify-center flex-wrap mt-4 mb-8 text-white">
-        {categories.map((category) => (
+        {Categories.map((category) => (
           <div
-            className="h-8 mr-2 px-3 py-1 bg-blue-400 text-center"
+            className={`h-8 mr-2 px-3 py-1 ${
+              projectsShown.category === category
+                ? "bg-blue-700 font-bold"
+                : "bg-blue-400"
+            } text-center cursor-pointer hover:bg-blue-300 hover:font-bold`}
             key={category}
+            onClick={() => changeCategory(category)}
           >
             {category}
           </div>
         ))}
       </nav>
       <div className="flex justify-around flex-wrap py-4">
-        {data.map((e, index) => {
+        {projectsShown.projectsArray.map((e, index) => {
           return (
             <SingleProject
               key={index}
